e0cebcc22f6212ed00ad7c5bb7d12ac502431266
winston kiprop <winstonekip50@gmail.com>
2022-06-04 14:59:33 +0300

    commit tree 00a2802bc380cd9b85a0fc8ae044e2f7d65329cc

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"]
+ 
+ const destructivelyAppendCat = cat => cats.push(cat);
+ const destructivelyPrependCat = cat => cats.unshift(cat)
+ const destructivelyRemoveLastCat = cat => cats.pop (cat)
+ const destructivelyRemoveFirstCat = cat => cats.shift(cat)
